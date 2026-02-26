7419d08082d79b777ef9f35b111417a7063219cd
naima2022326-lab <na4715645@gmail.com>
2026-02-26 09:03:14 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,16 +8,28 @@ const passwords = [
 ];
 
 function checkPassword() {
-  const input = document.getElementById("passwordInput").value.toLowerCase();
-  const error = document.getElementById("error");
-
+  const input = passwordInput.value.toLowerCase();
   if (passwords.includes(input)) {
-    document.getElementById("lockScreen").style.opacity = "0";
-    setTimeout(() => {
-      document.getElementById("lockScreen").style.display = "none";
-      document.getElementById("app").classList.remove("hidden");
-    }, 400);
+    fadeOut("lockScreen", "intro1");
   } else {
     error.textContent = "Incorrect password";
   }
+}
+
+function fadeOut(current, next) {
+  const c = document.getElementById(current);
+  c.style.opacity = "0";
+  setTimeout(() => {
+    c.classList.add("hidden");
+    document.getElementById(next).classList.remove("hidden");
+  }, 600);
+}
+
+function nextIntro(n) {
+  fadeOut(`intro${n-1}`, `intro${n}`);
+}
+
+function enterApp() {
+  document.getElementById("intro3").classList.add("hidden");
+  document.getElementById("app").classList.remove("hidden");
 }
