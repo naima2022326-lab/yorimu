4439d652f97f1f05c121a3aa30e778d64b8c9232
naima2022326-lab <na4715645@gmail.com>
2026-03-05 20:52:08 -0600

    Update extensions.js

--- yorimu-backend/extensions.js
+++ yorimu-backend/extensions.js
@@ -18,3 +18,20 @@ function searchAllSources(query) {
     });
   });
 }
+
+const fs = require("fs")
+const path = require("path")
+
+const EXT_DIR = "./extensions"
+
+function loadExtensions() {
+  if (!fs.existsSync(EXT_DIR)) return []
+
+  const files = fs.readdirSync(EXT_DIR)
+
+  return files.map(file => {
+    return require(path.join("../extensions", file))
+  })
+}
+
+module.exports = { loadExtensions }
