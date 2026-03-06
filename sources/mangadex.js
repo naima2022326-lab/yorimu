bc5f7216b99099465b0e4851eedc66efd33b2388
naima2022326-lab <na4715645@gmail.com>
2026-03-05 20:58:26 -0600

    Update mangadex.js

--- sources/mangadex.js
+++ sources/mangadex.js
@@ -26,3 +26,58 @@ registerExtension({
 
   }
 });
+
+const axios = require("axios")
+
+const BASE_URL = "https://api.mangadex.org"
+
+// SEARCH MANGA
+async function searchManga(title) {
+  const res = await axios.get(`${BASE_URL}/manga`, {
+    params: {
+      title: title,
+      limit: 10,
+      contentRating: ["safe","suggestive","erotica"],
+      includes: ["cover_art"]
+    }
+  })
+
+  return res.data.data
+}
+
+
+// GET CHAPTER LIST
+async function getChapters(mangaId) {
+  const res = await axios.get(`${BASE_URL}/chapter`, {
+    params: {
+      manga: mangaId,
+      translatedLanguage: ["en"],
+      order: { chapter: "asc" },
+      limit: 100
+    }
+  })
+
+  return res.data.data
+}
+
+
+// GET CHAPTER PAGES
+async function getPages(chapterId) {
+  const res = await axios.get(`${BASE_URL}/at-home/server/${chapterId}`)
+
+  const base = res.data.baseUrl
+  const hash = res.data.chapter.hash
+  const pages = res.data.chapter.data
+
+  const pageUrls = pages.map(page =>
+    `${base}/data/${hash}/${page}`
+  )
+
+  return pageUrls
+}
+
+module.exports = {
+  searchManga,
+  getChapters,
+  getPages
+}
